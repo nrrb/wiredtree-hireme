f876d4c8532ab2e3ea181a5e7ed155d92f383ecd
Nick Bennett <nick271828@gmail.com>
2013-05-02 18:24:46 -0500

    I got the job!

--- image_mosaic.py
+++ image_mosaic.py
@@ -17,9 +17,8 @@
 # |__/|__/_/_/   \___/\__,_/ /_/ /_/   \___/\___(_)
 
 from PIL import Image
-from os.path import splitext
+import os.path
 import argparse
-import pdb
 
 _DESCRIPTION = ('Decompose an image into chunks intended '
                 'to be printed on 4"x6" photo prints and '
@@ -54,12 +53,12 @@ def parse_arguments():
                         required=True,
                         type=int,
                         help='Max height of resulting poster, in inches.')
-    # parser.add_argument('-o',
-    #                     metavar='OUTPUT_PATH',
-    #                     dest='output_path',
-    #                     required=False,
-    #                     help=('Desired path for output files. This must '
-    #                           'exist! Defaults to current directory.'))
+    parser.add_argument('-o',
+                        metavar='OUTPUT_PATH',
+                        dest='output_path',
+                        required=False,
+                        help=('Desired path for output files. This must '
+                              'exist! Defaults to current directory.'))
     return parser.parse_args()
 
 
@@ -73,7 +72,8 @@ if __name__ == '__main__':
         print 'Oh dear, we seem to have run into a snag opening your file...'
         print e
         sys.exit(1)
-    basef, extf = splitext(args.image_path)
+    basef, extf = os.path.splitext(args.image_path)
+    output_path = args.output_path or '.'
 
     aspect_ratio = float(img.size[0]) / float(img.size[1])
 
@@ -96,9 +96,6 @@ if __name__ == '__main__':
         prints_wide = prints_wide + 1
     if poster_height % _PRINT_HEIGHT != 0:
         prints_high = prints_high + 1
-    # This then informs a new final poster size
-    # poster_width = prints_wide * _PRINT_WIDTH * _PRINT_DPI
-    # poster_height = prints_high * _PRINT_HEIGHT * _PRINT_DPI
 
     # Now start breaking the image into chunks and blowing them up
     chunk_width = float(img.size[0])/float(prints_wide)
@@ -110,15 +107,14 @@ if __name__ == '__main__':
             chunk_x1 = int(float(print_x) * chunk_width)
             chunk_x2 = int(float(print_x + 1) * chunk_width)
             chunk = img.crop((chunk_x1, chunk_y1, chunk_x2, chunk_y2))
-            # pdb.set_trace()
             chunk = chunk.resize((_PRINT_WIDTH * _PRINT_DPI,
                                   _PRINT_HEIGHT * _PRINT_DPI))
-            # pdb.set_trace()
             output_filename = '%s-%dx%d-print%dx%d%s' % (basef,
                                                          prints_high,
                                                          prints_wide,
                                                          print_y,
                                                          print_x,
                                                          extf)
+            output_filename = os.path.join(output_path, output_filename)
             chunk.save(output_filename)
             print output_filename
